5221c83a1fc55930658ed52c8add628b77730600
Developstar <deyeye99@gmail.com>
2022-07-27 19:42:08 +0100

    first commit

---
+++ pro.js
@@ -0,0 +1,35 @@
+let number = {
+    'mtn' : [0812, 0816, 0814],
+    'glo' : [0819, 0813, 0818],
+    'airtel' : [0817, 0811, 0815]
+}
+
+
+function netImage(){
+    let num = document.getElementById("phoneNum").value;
+    if (num == number[1[2]]) {
+         document.getElementById("image-container").style.backgroundImage = num;
+    }
+   
+}
+
+
+
+
+
+// const dis = document.getElementById("mtn-image");
+
+// dis.addEventListener()
+
+
+// const userInput = document.querySelector("input");
+
+// function displayImage(params) {
+    
+// }
+
+// input.oninput = function(){
+//     result.innerHTML = input.value;
+// };
+
+
