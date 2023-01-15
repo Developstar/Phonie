511d6ee31d32a29c4627a05678af57b79ac1000f
Developstar <deyeye99@gmail.com>
2023-01-15 04:51:16 +0100

    An header section component for Landing Page

---
+++ my-app/src/components/LandingPageCompo/LP_Header_Sec/Header.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="lp_header">
+    <div>Phonie</div>
+    <div>
+        <button>Request Early Access</button>   
+    </div>
+</div>
+</template>
+
+
+<script>
+export default{
+    name: 'Header'
+}
+</script>
+
+
+<style>
+</style>
