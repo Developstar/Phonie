492026688fa58bb8d3bc1771e053b466391379a1
Developstar <deyeye99@gmail.com>
2023-01-16 14:47:51 +0100

    Header section styled

--- my-app/src/components/LandingPageCompo/HeaderSection.vue
+++ my-app/src/components/LandingPageCompo/HeaderSection.vue
@@ -1,7 +1,7 @@
 <template>
-<div class="lp_header">
-    <div>Phonie</div>
-    <div>
+<div class="HeaderSection">
+    <div class="logo">Phonie</div>
+    <div class="AccessButton">
         <button>Request Early Access</button>   
     </div>
 </div>
@@ -16,4 +16,31 @@ export default{
 
 
 <style>
+.HeaderSection{
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+    background: #22253e;
+    color: white;
+    height: 5rem;
+    
+}
+
+.logo{
+    font-size: 2rem;
+    font-weight: bold;
+    margin-left: 1rem;
+}
+
+.AccessButton{
+    margin-right: 1rem;
+}
+
+.AccessButton button{
+    background: white;
+    border: none;
+    padding: 0.7rem;
+    border-radius: 0.4rem;
+    
+}
 </style>
