f3d7bfdccc63fea18897d3ba8cc88516ebfd997d
Developstar <deyeye99@gmail.com>
2023-01-16 14:53:31 +0100

    default page margin and padding removed

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div >
     <LandingPage />
   </div>
   
@@ -17,12 +17,15 @@ export default {
 </script>
 
 <style>
+body{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
