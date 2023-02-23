4752153ec25a7b8027f4480638e9301c448667a0
Developstar <deyeye99@gmail.com>
2023-02-23 20:50:31 +0100

    Created the landing Page component

---
+++ my-app/src/components/LandingPageCompo/LandingPageView.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <div class="Content-Wrapper">
+            <h1>Connecting you to the world, one click at a time.</h1>
+            <p>The world has become increasingly connected in recent years, with the internet and mobile phones playing a crucial role in facilitating communication and interaction across borders. A telecommunications website acts as a gateway to this connected world, providing users with access to various communication technologies and services that allow them to connect with anyone, anywhere, at any time. With just a click of a button, users can access high-speed internet, make phone calls, send messages, and even stream their favorite TV shows and movies. At its core, a telecommunications website is all about connecting people, and with its help, individuals and businesses can stay connected and thrive in today's hyper-connected world.</p>
+            <button></button>
+        </div>
+        <div class="illustration-wrapper">
+            <img src="../../assets/image-hero-desktop.png">
+        </div>
+    </section>
+</template>
+
+<script>
+
+export default{
+    name: "LandingPageView"
+}
+</script>
+
+<style>
+
+</style>
