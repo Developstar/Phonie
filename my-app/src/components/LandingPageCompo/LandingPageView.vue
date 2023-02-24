c39d4026482712002b04b98b25060dcb4a9a7d1e
Developstar <deyeye99@gmail.com>
2023-02-24 23:10:04 +0100

    changed the slogan and the paragraph

--- my-app/src/components/LandingPageCompo/LandingPageView.vue
+++ my-app/src/components/LandingPageCompo/LandingPageView.vue
@@ -2,19 +2,12 @@
   <section class="page_wrapper">
     <div class="Content-Wrapper">
       <div class="content">
-        <h1>Connecting you to the world, one click at a time.</h1>
+        <h1 class="slogan">
+            <span>Connect</span> 
+            <span>with the world, one click at a time.</span>
+        </h1>
         <p>
-          The world has become increasingly connected in recent years, with the
-          internet and mobile phones playing a crucial role in facilitating
-          communication and interaction across borders. A telecommunications
-          website acts as a gateway to this connected world, providing users
-          with access to various communication technologies and services that
-          allow them to connect with anyone, anywhere, at any time. With just a
-          click of a button, users can access high-speed internet, make phone
-          calls, send messages, and even stream their favorite TV shows and
-          movies. At its core, a telecommunications website is all about
-          connecting people, and with its help, individuals and businesses can
-          stay connected and thrive in today's hyper-connected world.
+            Helping individuals and businesses stay connected and thrive in today's hyper-connected world
         </p>
         <button></button>
     </div>
@@ -45,14 +38,21 @@ export default {
 .Content-Wrapper {
   border: 1px solid red;
   height: 70vh;
-  text-align: center;
+  
   width: 65%;
 }
 
 .content {
   border: 1px solid red;
-  width: 80%;
+  width: 95%;
   margin-top: 5rem;
+}
+
+.slogan{
+    display: flex;
+    flex-direction: column;
+    font-weight: bolder;
+    font-size: 2.5rem;
 }
 
 .illustration-wrapper {
